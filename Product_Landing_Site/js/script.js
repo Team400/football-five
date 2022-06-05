f8ea835d8210608d09966d2a5eb809da57c42618
Carl <cvrlix@gmail.com>
2022-06-05 11:05:24 -0400

    personas formatted with new template

--- Product_Landing_Site/js/script.js
+++ Product_Landing_Site/js/script.js
@@ -1,10 +1,8 @@
 "use strict";
 
-//const header = "Our inspirations";
-
-function doThat(obj)
+function animate(obj)
 {
-    let element = document.getElementById("header");
+    let element = document.getElementById("persona");
     element.innerText = "";
     let timer = setInterval(step, 40);
     let i = 0;
@@ -19,42 +17,3 @@ function doThat(obj)
     }
 
 }
-
-function slide(id)
-{
-    let i = parseInt(id.charAt(8));
-    let element = document.getElementById(id)
-    element.style.display = "none";
-
-    let persona = Math.ceil(Math.random()*2);
-    while (persona === i)
-        persona = Math.ceil(Math.random()*2);
-
-    console.log("persona-"+persona);
-    element = document.getElementById("persona-"+persona);
-    
-    //parent.style.visibility = "visible";
-    console.log(parent);
-    element.style.visibility = "visible";
-    element.style.display = "block";
-
-    element.classList.add("row", "justify-content-center");
-
-
-
-}
-function flip(id)
-{
-    let element = document.getElementById(id);
-    //element.style.visibility = "hidden";
-    element.style.display = "none";
-    console.log("hi there");
-    console.log(element);
-}
-
-function load()
-{
-    window.location = "persona2.html";
-    console.log("hi there");
-}
-
