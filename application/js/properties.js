5b89afe232c8d7188646befd4e851d05641dc61f
Andrew Gordon <Agordon03@bellarmine.edu>
2022-06-21 23:10:12 -0400

    6/21/22- Andrew- Updated Dashboard page- 3 hrs

--- application/js/properties.js
+++ application/js/properties.js
@@ -9,12 +9,12 @@ function DraftPlayer(playerNumber)
     show();
     if(rosterNumber<11)
     {
-        let P = document.getElementById("position"+playerNumber);
-        let position = P.innerHTML;
-        let N = document.getElementById("name"+playerNumber);
-        let name = N.innerHTML;
-        let S = document.getElementById("college"+playerNumber);
-        let school = S.innerHTML;
+        var P = document.getElementById("position"+playerNumber);
+        var position = P.innerHTML;
+        var N = document.getElementById("name"+playerNumber);
+        var name = N.innerHTML;
+        var S = document.getElementById("college"+playerNumber);
+        var school = S.innerHTML;
         addToRoster(position, name, school, rosterNumber);
         changeRosterNum(rosterNumber);
        element.remove();
@@ -29,19 +29,19 @@ function DraftPlayer(playerNumber)
 
 function addToRoster(position, name, school, number)
 {
-    let player = {pos: position, nm: name, coll: school}
+    var player = {pos: position, nm: name, coll: school}
     roster[number-1] = player;
     alert("\tYou drafted " + roster[number-1].pos + " " + roster[number-1].nm +", " + roster[number-1].coll+ "!");
-    let node = document.createElement("p");
-    let text = document.createTextNode(position);
+    var node = document.createElement("p");
+    var text = document.createTextNode(position);
     node.appendChild(text);
     document.getElementById("yourPlayerPositions").appendChild(node);
-    let node1 = document.createElement("p");
-    let text1 = document.createTextNode(name);
+    var node1 = document.createElement("p");
+    var text1 = document.createTextNode(name);
     node1.appendChild(text1);
     document.getElementById("yourPlayerNames").appendChild(node1);
-    let node2 = document.createElement("p");
-    let text2 = document.createTextNode(school);
+    var node2 = document.createElement("p");
+    var text2 = document.createTextNode(school);
     node2.appendChild(text2);
     document.getElementById("yourPlayerSchool").appendChild(node2);
 
@@ -72,4 +72,10 @@ function show()
     document.getElementById("names").style.display = "block";
     document.getElementById("yourPlayerSchool").style.display = "block";
     document.getElementById("school").style.display = "block";
+}
+
+function refreshRoster(){
+ alert(P);
+ 
+   
 }
